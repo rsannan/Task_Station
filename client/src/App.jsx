92f79c1a644f60a4732e312aec144e3a4c17c4b6
Reginald Shawn Annan <shawnrannan@gmail.com>
2023-04-09 16:19:39 +0100

    create client vite react folder

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import { Navbar } from "./Components";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import logo from "../../assets/logo.png";
+export default function Navbar() {
+  return (
+    <>
+      <nav className="navbar bg-dark navbar-expand-lg">
+        <div className="container-fluid fw-semibold">
+          <a className="navbar-brand text-white " href="#">
+            <img src={logo} alt="logo" className="w-25" />
+            Task-Station
+          </a>
+          <ul className="navbar-nav">
+            <li className="navbar-item">
+              <a href="#" className="nav-link active text-white">
+                Home
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+}
