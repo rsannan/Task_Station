6e6074c602fff9d77899a5145751234193b378db
Reginald Shawn Annan <shawnrannan@gmail.com>
2023-04-20 17:09:13 +0100

    add task card

--- client/src/Components/Dashboard/Dashboard.jsx
+++ client/src/Components/Dashboard/Dashboard.jsx
@@ -1,11 +1,24 @@
 import "./dashboard.css";
+import logo from "../../assets/addtask1.png";
 export default function Dashboard() {
   return (
     <div className="dashboard">
       <div className="dashcard">
-        <h3>Title</h3>
-        <p>description</p>
-        <h5>Time and Date</h5>
+        <div className="d-inline-block col-6 mx-auto h-100 w-100 addtask">
+          <a
+            className="btn btn-outline-info h-100 w-100"
+            role="button"
+            href="/addtask"
+          >
+            <img
+              src={logo}
+              width="50%"
+              alt="Add task img"
+              style={{ marginTop: "102px" }}
+            />
+            <h3>Add Task</h3>
+          </a>
+        </div>
       </div>
     </div>
   );
