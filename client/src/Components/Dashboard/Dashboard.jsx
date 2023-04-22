6670d70b6970633bfedf1a677fee854f47cbbb09
Reginald Shawn Annan <shawnrannan@gmail.com>
2023-04-22 10:16:34 +0100

    fix sidnav final position

--- client/src/Components/Dashboard/Dashboard.jsx
+++ client/src/Components/Dashboard/Dashboard.jsx
@@ -2,42 +2,44 @@ import "./dashboard.css";
 import logo from "../../assets/addtask1.png";
 export default function Dashboard() {
   return (
-    <div className="dashboard">
-      <div className="dashcard">
-        <div className="d-inline-block col-6 mx-auto h-100 w-100 addtask">
-          <a
-            className="btn btn-outline-info h-100 w-100"
-            role="button"
-            href="/addtask"
-          >
-            <img
-              src={logo}
-              width="50%"
-              alt="Add task img"
-              style={{ marginTop: "102px" }}
-            />
-            <h3>Add Task</h3>
-          </a>
+    <div className="dashcon">
+      <div className="dashboard">
+        <div className="dashcard">
+          <div className="d-inline-block col-6 mx-auto h-100 w-100 addtask">
+            <a
+              className="btn btn-outline-info h-100 w-100"
+              role="button"
+              href="/addtask"
+            >
+              <img
+                src={logo}
+                width="50%"
+                alt="Add task img"
+                style={{ marginTop: "102px" }}
+              />
+              <h3>Add Task</h3>
+            </a>
+          </div>
         </div>
-      </div>
-      <div className="dashcard">
-        <div className="d-flex flex-row-reverse">
-          <button
-            className="btn btn-danger btn-sm rounded-0 p-2"
-            type="button"
-            data-toggle="tooltip"
-            title="Delete"
-          >
-            <i className="fa fa-trash"></i>
-          </button>
-          <button
-            className="btn btn-success btn-sm rounded-0 p-2"
-            type="button"
-            data-toggle="tooltip"
-            title="Edit"
-          >
-            <i className="fa fa-edit"></i>
-          </button>
+        <div className="dashcard">
+          <div className="d-flex flex-row-reverse">
+            <button
+              className="btn btn-danger btn-sm rounded-0 p-2"
+              type="button"
+              data-toggle="tooltip"
+              title="Delete"
+            >
+              <i className="fa fa-trash"></i>
+            </button>
+            <button
+              className="btn btn-success btn-sm rounded-0 p-2"
+              type="button"
+              data-toggle="tooltip"
+              title="Edit"
+            >
+              <i className="fa fa-edit"></i>
+            </button>
+          </div>
         </div>
       </div>
     </div>
--- client/src/Components/SideNav/SideNav.jsx
+++ client/src/Components/SideNav/SideNav.jsx
@@ -4,29 +4,29 @@ export default function SideNav() {
   return (
     <nav
       id="sidebarMenu"
-      class="collapse d-lg-block sidebar collapse bg-white vh-100"
+      className="collapse d-lg-block sidebar collapse bg-white vh-100"
     >
-      <div class="position-static">
-        <div class="list-group list-group-flush mx-3 mt-4">
+      <div className="position-static">
+        <div className="list-group list-group-flush mx-3 mt-4">
           <a
-            class="list-group-item list-group-item-action py-2 ripple"
+            className="list-group-item list-group-item-action py-2 ripple"
             aria-current="true"
           >
-            <i class="fas fa-tachometer-alt fa-fw me-3"></i>
+            <i className="fas fa-tachometer-alt fa-fw me-3"></i>
             <span>Work Boards</span>
           </a>
           <a
             href="#"
-            class="list-group-item list-group-item-action py-2 ripple"
+            className="list-group-item list-group-item-action py-2 ripple"
           >
-            <i class="fa-solid fa-plus me-3"></i>
+            <i className="fa-solid fa-plus me-3"></i>
             <span>Add Board</span>
           </a>
           <a
             href="#"
-            class="list-group-item list-group-item-action py-2 ripple"
+            className="list-group-item list-group-item-action py-2 ripple"
           >
-            <i class="fas fa-money-bill fa-fw me-3"></i>
+            <i className="fas fa-money-bill fa-fw me-3"></i>
             <span>Sales</span>
           </a>
         </div>
